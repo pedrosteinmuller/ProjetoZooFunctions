3beed69fa09f1ae562adc7e45c4eac1e6d683caf
Pedro <pedrosteinmuller10105@hotmail.com>
2022-07-28 22:14:38 -0300

    resolucao requisito 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,17 @@
 const data = require('../data/zoo_data');
 
-function countAnimals(animal) {
-  // seu código aqui
-}
+const countAnimals = (animal) => {
+  const { species } = data;
+  if (!animal) {
+    return species.reduce((acc, curr) => {
+      acc[curr.name] = curr.residents.length;
+      return acc;
+    }, {});
+  }
+  const { residents } = species.find((item) => item.name === animal.specie);
+  return !animal.sex
+    ? residents.length
+    : residents.filter((element) => element.sex === animal.sex).length;
+};
 
 module.exports = countAnimals;
