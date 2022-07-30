a08d9997c6b78b4f7d34829153d5f4b0d9cf87b6
Pedro <pedrosteinmuller10105@hotmail.com>
2022-07-30 17:40:59 -0300

    resolucao requisito 9

--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,11 +1,55 @@
-// const data = require('../data/zoo_data');
-// const { hours, species } = require('../data/zoo_data');
+const data = require('../data/zoo_data');
 
-// const days = Object.keys(hours);
-// const hour = Object.values(hours)
+// feito com ajuda do Rafael Sampaio da turma 24 tribo A
 
-// const getSchedule = (scheduleTarget) => {
-//  return data.species.find((item) => item.name === scheduleTarget).availability
-//   if (!scheduleTarget)
-// }
-// module.exports = getSchedule;
+const daysWeek = [
+  'Tuesday',
+  'Wednesday',
+  'Thursday',
+  'Friday',
+  'Saturday',
+  'Sunday',
+  'Monday',
+];
+const listAn = data.species.map((element) => element.name);
+
+const hour1 = (day) => {
+  if (day === 'Monday') {
+    return 'CLOSED';
+  }
+  return `Open from ${data.hours[day].open}am until ${data.hours[day].close}pm`;
+};
+
+const exh = (day) => {
+  if (day === 'Monday') {
+    return 'The zoo will be closed!';
+  }
+  return data.species.reduce((acc, currValue) => {
+    if (currValue.availability.includes(day)) {
+      acc.push(currValue.name);
+    }
+    return acc;
+  }, []);
+};
+
+const getSchedule = (scheduleTarget) => {
+  if (daysWeek.includes(scheduleTarget)) {
+    return {
+      [scheduleTarget]: {
+        officeHour: hour1(scheduleTarget),
+        exhibition: exh(scheduleTarget),
+      },
+    };
+  }
+  if (!scheduleTarget || (!daysWeek.includes(scheduleTarget) && !listAn.includes(scheduleTarget))) {
+    return daysWeek.reduce(
+      (acc, currValue) => ({
+        ...acc,
+        [currValue]: { officeHour: hour1(currValue), exhibition: exh(currValue),
+        },
+      }), {},
+    );
+  }
+  return data.species.find((item) => item.name === scheduleTarget).availability;
+};
+module.exports = getSchedule;
